b5991a17127f85ee4935d827abbf88c8524923da
ChieveiT <javachieveit@163.com>
2016-11-22 00:51:23 +0800

    fix types.js

--- src/types.js
+++ src/types.js
@@ -2,5 +2,6 @@ import { PropTypes } from 'react';
 
 export let routesShape = PropTypes.shape({
   match: PropTypes.func.isRequired,
-  link: PropTypes.func.isRequired
+  linkByName: PropTypes.func.isRequired,
+  linkByPath: PropTypes.func.isRequired
 });
